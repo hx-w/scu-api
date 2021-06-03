f5ab24c4b3df072a89fdad35a690f4fe6a2f9992
hx-w <17361019875@163.com>
2021-06-03 10:21:37 +0800

    edit: update tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,6 +4,6 @@ import scu_api
 
 bot = scu_api.get_student(scu_api.Student_Type.UNDERGRADUATE)
 
-def test_warning():
-    resp = bot.get_student_name()
-    assert not resp.is_ok()
+def test_captcha():
+    resp = bot.get_captcha()
+    assert resp.is_ok()
